5503d0de124a1c47e30796c335e571482ff5231a
Stuart Dootson <stuart.dootson@rolls-royce.com>
2021-12-02 16:24:09 +0000

    Simplify solution using `windows` method

--- day-one/src/main.rs
+++ day-one/src/main.rs
@@ -1,6 +1,3 @@
-#![feature(iter_zip)]
-use std::iter::zip;
-
 pub fn parse(input: &str) -> Vec<usize> {
     input.lines().flat_map(|s| s.parse::<usize>()).collect()
 }
@@ -10,27 +7,19 @@ fn main() {
     let measurements = parse(input_string);
 
     {
-        let count_larger = zip(&measurements, &measurements[1..])
-            .map(|(prev, current)| if current > prev { 1 } else { 0 })
-            .sum::<i32>();
+        let count_larger = measurements.windows(2).filter(|s| s[1] > s[0]).count();
         println!("Day  1 part 1 - depth increase count = {}", count_larger);
     }
     {
-        let count_larger = zip(
-            zip(&measurements, &measurements[1..]),
-            zip(&measurements[2..], &measurements[3..]),
-        )
-        .map(|((a, b), (c, d))| {
-            let prev = a + b + c;
-            let current = b + c + d;
-
-            if current > prev {
-                1
-            } else {
-                0
-            }
-        })
-        .sum::<i32>();
+        // Rolling average = m[i,i+1,i+2] where the oldest point i. So - at time
+        // t, the current rolling average is `m[t-2]+m[t-1]+m[t]` and the
+        // previous one is `m[t-3]+m[t-2]+m[t-1]`.
+        //
+        // We want to see if `current>previous`, i.e. `(m[t-2]+m[t-1]+m[t]) >
+        // (m[t-3]+m[t-2]+m[t-1])`. The terms `m[t-2]` and `m[t-1]` can be
+        // eliminated from both sides of this, so we just need to see if `m[t] >
+        // m[t-3]`.
+        let count_larger = measurements.windows(4).filter(|s| s[3] > s[0]).count();
         println!(
             "Day  1 part 2 - rolling average depth increase count = {}",
             count_larger
