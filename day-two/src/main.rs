0786f5f9962a72ddc1e45bf7dfd5dcef932e42d8
Stuart Dootson <stuart.dootson@rolls-royce.com>
2021-12-02 17:11:09 +0000

    Done day2 part 2

--- day-two/src/main.rs
+++ day-two/src/main.rs
@@ -1,20 +1,17 @@
 #[derive(Debug)]
-struct Depth(i32);
+pub struct Depth(i32);
 
 #[derive(Debug)]
-struct Horizontal(i32);
+pub struct Horizontal(i32);
 
 #[derive(Debug)]
-struct Location(Horizontal, Depth);
-
-#[derive(Debug)]
-enum Motion {
+pub enum Motion {
     Up(i32),
     Down(i32),
     Forward(i32),
 }
 
-fn parse_motion(line: &str) -> Motion {
+pub fn parse_motion(line: &str) -> Motion {
     if line.starts_with("forward ") {
         Motion::Forward(line[8..].parse::<i32>().unwrap())
     } else if line.starts_with("down ") {
@@ -26,31 +23,75 @@ fn parse_motion(line: &str) -> Motion {
     }
 }
 
-fn parse(input: &str) -> Vec<Motion> {
+pub fn parse(input: &str) -> Vec<Motion> {
     input.lines().map(|s| parse_motion(s)).collect()
 }
 
-fn perform_motion(start: Location, motion: &Motion) -> Location {
-    match motion {
-        Motion::Down(depth_change) => Location(start.0, Depth(start.1 .0 + depth_change)),
-        Motion::Up(depth_change) => Location(start.0, Depth(start.1 .0 - depth_change)),
-        Motion::Forward(pos_change) => Location(Horizontal(start.0 .0 + pos_change), start.1),
+pub mod part1 {
+    use super::*;
+
+    #[derive(Debug)]
+    pub struct Location(pub Horizontal, pub Depth);
+
+    pub fn perform_motion(start: Location, motion: &Motion) -> Location {
+        match motion {
+            Motion::Down(depth_change) => Location(start.0, Depth(start.1 .0 + depth_change)),
+            Motion::Up(depth_change) => Location(start.0, Depth(start.1 .0 - depth_change)),
+            Motion::Forward(pos_change) => Location(Horizontal(start.0 .0 + pos_change), start.1),
+        }
+    }
+
+    pub fn perform_motions(start: Location, motions: &[Motion]) -> Location {
+        motions.iter().fold(start, &perform_motion)
     }
 }
 
-fn perform_motions(start: Location, motions: &[Motion]) -> Location {
-    motions.iter().fold(start, &perform_motion)
+pub mod part2 {
+    use super::*;
+
+    #[derive(Debug)]
+    pub struct Aim(pub i32);
+
+    #[derive(Debug)]
+    pub struct Location(pub Horizontal, pub Depth, pub Aim);
+
+    pub fn perform_motion(start: Location, motion: &Motion) -> Location {
+        match motion {
+            Motion::Down(aim_change) => Location(start.0, start.1, Aim(start.2 .0 + aim_change)),
+            Motion::Up(aim_change) => Location(start.0, start.1, Aim(start.2 .0 - aim_change)),
+            Motion::Forward(pos_change) => Location(
+                Horizontal(start.0 .0 + pos_change),
+                Depth(start.1 .0 + (pos_change * start.2 .0)),
+                start.2,
+            ),
+        }
+    }
+
+    pub fn perform_motions(start: Location, motions: &[Motion]) -> Location {
+        motions.iter().fold(start, &perform_motion)
+    }
 }
 
 fn main() {
     let input_string = include_str!("../input.txt");
     let motions = parse(input_string);
 
-    let end = perform_motions(Location(Horizontal(0), Depth(0)), &motions);
-
-    println!(
-        "Day  2 part 1 - end location is {:?}, multiplied = {}",
-        end,
-        end.0 .0 * end.1 .0
-    );
+    {
+        use part1::*;
+        let end = perform_motions(Location(Horizontal(0), Depth(0)), &motions);
+        println!(
+            "Day  2 part 1 - end location is {:?}, multiplied = {}",
+            end,
+            end.0 .0 * end.1 .0
+        );
+    }
+    {
+        use part2::*;
+        let end = perform_motions(Location(Horizontal(0), Depth(0), Aim(0)), &motions);
+        println!(
+            "Day  2 part 2 - end location is {:?}, multiplied = {}",
+            end,
+            end.0 .0 * end.1 .0
+        );
+    }
 }
