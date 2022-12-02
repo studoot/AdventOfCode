1aae2100a31fc07296f2961e75cf50f581378d5a
Stuart Dootson <stuart.dootson@rolls-royce.com>
2022-12-02 12:20:16 +0000

    Done 2022 day one

---
+++ 2022/src/day1/mod.rs
@@ -0,0 +1,63 @@
+use itertools::Itertools;
+
+type Calories = Vec<usize>;
+
+fn get_calories(s: &str) -> Calories {
+    s.lines()
+        .group_by(|s| s.is_empty())
+        .into_iter()
+        .filter_map(|(is_separator, g)| {
+            (!is_separator).then(|| g.map(|s| s.parse::<usize>().unwrap()).sum())
+        })
+        .collect::<Vec<_>>()
+}
+
+fn find_max_calories(c: &Calories) -> usize {
+    *c.iter().max().unwrap_or(&0)
+}
+
+fn find_top_3_calories(c: &mut Calories) -> usize {
+    c.sort();
+    c.rchunks(3).next().unwrap().iter().sum()
+}
+
+#[cfg(test)]
+const TEST_INPUT_STRING: &str = "\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+#[test]
+fn test_part1() {
+    let c = get_calories(TEST_INPUT_STRING);
+    assert_eq!(find_max_calories(&c), 24_000);
+}
+
+#[test]
+fn test_part2() {
+    let mut c = get_calories(TEST_INPUT_STRING);
+    assert_eq!(find_top_3_calories(&mut c), 45_000);
+}
+
+pub fn run() {
+    let input_string = include_str!("./input.txt");
+    let mut c = get_calories(input_string);
+    let part1_answer = find_max_calories(&c);
+    let part2_answer = find_top_3_calories(&mut c);
+    println!(
+        "Day 1, part 1 = {part1_answer} [{}], part 2 = {part2_answer} [{}]",
+        part1_answer == 71924,
+        part2_answer == 210406
+    );
+}
---
+++ 2022/src/main.rs
@@ -0,0 +1,68 @@
+use std::env;
+
+mod day1;
+// mod day2;
+// mod day3;
+// mod day4;
+// mod day5;
+// mod day6;
+// mod day7;
+// mod day8;
+// mod day9;
+// mod day10;
+// mod day11;
+// mod day12;
+// mod day13;
+// mod day14;
+// mod day15;
+// mod day16;
+// mod day17;
+// mod day18;
+// mod day19;
+// mod day20;
+// mod day21;
+// mod day22;
+// mod day23;
+// mod day24;
+// mod day25;
+
+fn dispatch_day(day_number: usize) {
+    match day_number {
+        1 => day1::run(),
+        // 2 => day2::run(),
+        // 3 => day3::run(),
+        // 4 => day4::run(),
+        // 5 => day5::run(),
+        // 6 => day6::run(),
+        // 7 => day7::run(),
+        // 8 => day8::run(),
+        // 9 => day9::run(),
+        // 10 => day10::run(),
+        // 11 => day11::run(),
+        // 12 => day12::run(),
+        // 13 => day13::run(),
+        // 14 => day14::run(),
+        // 15 => day15::run(),
+        // 16 => day16::run(),
+        // 17 => day17::run(),
+        // 18 => day18::run(),
+        // 19 => day19::run(),
+        // 20 => day20::run(),
+        // 21 => day21::run(),
+        // 22 => day22::run(),
+        // 23 => day23::run(),
+        // 24 => day24::run(),
+        // 25 => day25::run(),
+        _ => println!("Day {day_number} not yet implemented"),
+    }
+}
+
+fn main() {
+    if let Some(day_string) = env::args().nth(1) {
+        dispatch_day(day_string.parse::<usize>().unwrap());
+    }else {
+        for day_number in 1..=25 {
+            dispatch_day(day_number);
+        }
+    }
+}
