307df23887cfdb60f2b5fb86f598a3c178cbed77
Stuart Dootson <stuart.dootson@rolls-royce.com>
2022-12-05 19:37:29 +0000

    Done 2022 day five

---
+++ 2022/src/day5/mod.rs
@@ -0,0 +1,116 @@
+use std::string::String;
+use std::vec::Vec;
+
+type Stack = Vec<char>;
+type Stacks = Vec<Stack>;
+
+#[derive(Debug)]
+struct Move {
+    from: usize,
+    to: usize,
+    count: usize,
+}
+type Moves = Vec<Move>;
+
+fn parse_stacks(s: &str) -> Stacks {
+    let mut line_iter = s.lines().rev();
+    let stack_count = line_iter.next().unwrap().split_whitespace().count();
+    let mut stacks = vec![Stack::new(); stack_count];
+    for line in line_iter {
+        for (stack_index, stack) in stacks.iter_mut().enumerate() {
+            let string_index = (stack_index * 4) + 1;
+            let _crate = line.chars().nth(string_index).unwrap();
+            if !_crate.is_whitespace() {
+                stack.push(_crate);
+            }
+        }
+    }
+    stacks
+}
+
+fn parse_move(s: &str) -> Move {
+    let bits = s.split_whitespace().collect::<Vec<_>>();
+    if bits.len() != 6 {
+        panic!("Wibble - {s} => {:?}", bits);
+    }
+    Move { count: bits[1].parse().unwrap(), from: bits[3].parse().unwrap(), to: bits[5].parse().unwrap() }
+}
+
+fn parse_moves(s: &str) -> Moves {
+    s.lines().map(parse_move).collect::<_>()
+}
+
+fn perform_move_part1(stacks: &mut Stacks, m: Move) {
+    for _ in 0..m.count {
+        if let Some(char_to_move) = stacks[m.from - 1].pop() {
+            stacks[m.to - 1].push(char_to_move);
+        } else {
+            panic!("Empty stack!!!!");
+        }
+    }
+}
+
+fn perform_move_part2(stacks: &mut Stacks, m: Move) {
+    let split_pos = stacks[m.from - 1].len() - m.count;
+    let mut to_move = stacks[m.from - 1].split_off(split_pos);
+    stacks[m.to - 1].append(&mut to_move);
+}
+
+fn parse(s: &str) -> (Stacks, Moves) {
+    if let Some((stacks, moves)) = s.replace("\r\n", "\n").split_once("\n\n") {
+        (parse_stacks(stacks), parse_moves(moves))
+    } else {
+        (Vec::new(), Vec::new())
+    }
+}
+
+fn evaluate(s: &str, perform_move: fn(stacks: &mut Stacks, m: Move)) -> String {
+    let (mut stacks, moves) = parse(s);
+    for m in moves {
+        perform_move(&mut stacks, m);
+    }
+    stacks
+        .into_iter()
+        .map(|s| *s.last().unwrap())
+        .collect::<String>()
+}
+
+fn part1_evaluate(s: &str) -> String {
+    evaluate(s, perform_move_part1)
+}
+
+fn part2_evaluate(s: &str) -> String {
+    evaluate(s, perform_move_part2)
+}
+
+#[cfg(test)]
+const TEST_INPUT_STRING: &str = "\
+\x20   [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
+
+#[cfg(test)]
+#[test]
+fn test_part1() {
+    assert_eq!(part1_evaluate(TEST_INPUT_STRING), "CMZ");
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2_evaluate(TEST_INPUT_STRING), "MCD");
+}
+
+pub fn run() -> Option<(String, bool, String, bool)> {
+    let input_string = include_str!("./input.txt");
+    let part1_answer = part1_evaluate(input_string);
+    let part1_good = part1_answer == "SHMSDGZVC";
+    let part2_answer = part2_evaluate(input_string);
+    let part2_good = part2_answer == "VRZGHDFBQ";
+    Some((part1_answer, part1_good, part2_answer, part2_good))
+}
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,10 +1,11 @@
 use std::env;
+use std::fmt::Display;
 
 mod day1;
 mod day2;
 mod day3;
 mod day4;
-// mod day5;
+mod day5;
 // mod day6;
 // mod day7;
 // mod day8;
@@ -26,7 +27,7 @@ mod day4;
 // mod day24;
 // mod day25;
 
-fn time(day_number: usize, f: &dyn Fn() -> Option<(usize, bool, usize, bool)>) {
+fn time<T: Display>(day_number: usize, f: &dyn Fn() -> Option<(T, bool, T, bool)>) {
     let now = std::time::Instant::now();
     let output = f();
     let duration = now.elapsed().as_micros();
@@ -39,37 +40,34 @@ fn time(day_number: usize, f: &dyn Fn() -> Option<(usize, bool, usize, bool)>) {
 
 fn dispatch_day(day_number: usize) {
     let oob = || None;
-    time(
-        day_number,
-        match day_number {
-            1 => &day1::run,
-            2 => &day2::run,
-            3 => &day3::run,
-            4 => &day4::run,
-            // 5 => day5::run,
-            // 6 => day6::run,
-            // 7 => day7::run,
-            // 8 => day8::run,
-            // 9 => day9::run,
-            // 10 => day10::run,
-            // 11 => day11::run,
-            // 12 => day12::run,
-            // 13 => day13::run,
-            // 14 => day14::run,
-            // 15 => day15::run,
-            // 16 => day16::run,
-            // 17 => day17::run,
-            // 18 => day18::run,
-            // 19 => day19::run,
-            // 20 => day20::run,
-            // 21 => day21::run,
-            // 22 => day22::run,
-            // 23 => day23::run,
-            // 24 => day24::run,
-            // 25 => day25::run,
-            _ => &oob,
-        },
-    )
+    match day_number {
+        1 => time(day_number, &day1::run),
+        2 => time(day_number, &day2::run),
+        3 => time(day_number, &day3::run),
+        4 => time(day_number, &day4::run),
+        5 => time(day_number, &day5::run),
+        // 6 => time(day_number, &day6::run),
+        // 7 => time(day_number, &day7::run),
+        // 8 => time(day_number, &day8::run),
+        // 9 => time(day_number, &day9::run),
+        // 10 => time(day_number, &day10::run),
+        // 11 => time(day_number, &day11::run),
+        // 12 => time(day_number, &day12::run),
+        // 13 => time(day_number, &day13::run),
+        // 14 => time(day_number, &day14::run),
+        // 15 => time(day_number, &day15::run),
+        // 16 => time(day_number, &day16::run),
+        // 17 => time(day_number, &day17::run),
+        // 18 => time(day_number, &day18::run),
+        // 19 => time(day_number, &day19::run),
+        // 20 => time(day_number, &day20::run),
+        // 21 => time(day_number, &day21::run),
+        // 22 => time(day_number, &day22::run),
+        // 23 => time(day_number, &day23::run),
+        // 24 => time(day_number, &day24::run),
+        // 25 => time(day_number, &day25::run),
+        _ => time::<bool>(day_number, &oob),
+    }
 }
 
 fn main() {
