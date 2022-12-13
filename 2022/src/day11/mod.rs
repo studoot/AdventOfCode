8f6a87345a33e0852392d68f52e70c13fcbd4a3e
Stuart Dootson <stuart.dootson@rolls-royce.com>
2022-12-13 12:50:09 +0000

    Optimised 2022 day eleven - don't move items between monkeys

--- 2022/src/day11/mod.rs
+++ 2022/src/day11/mod.rs
@@ -5,6 +5,7 @@ use itertools::Itertools;
 #[derive(Debug, Clone, Copy)]
 struct Item {
     worry_level: usize,
+    owner: usize,
 }
 
 #[derive(Debug)]
@@ -88,15 +89,26 @@ struct ThrowTo {
     if_true: usize,
     if_false: usize,
 }
+
+impl ThrowTo {
+    fn throw(&self, item: &Item) -> usize {
+        if item.worry_level % self.divisor == 0 {
+            self.if_true
+        } else {
+            self.if_false
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Monkey {
     id: usize,
-    items: Vec<Item>,
+    initial_items: Vec<Item>,
     inspection: Inspection,
     throw_to: ThrowTo,
     inspection_count: usize,
-    worry_div_factor:usize,
-    worry_mod_factor:usize,
+    worry_div_factor: usize,
+    worry_mod_factor: usize,
 }
 
 impl Monkey {
@@ -104,24 +116,17 @@ impl Monkey {
         item.worry_level = self.inspection.inspect(item.worry_level);
         self.inspection_count += 1;
     }
-    fn process_item(&mut self) -> Option<(usize, Item)> {
-        self.items.pop().map(|mut item| {
-            self.inspect(&mut item);
-            item.worry_level = (item.worry_level / self.worry_div_factor) % self.worry_mod_factor;
-            let throwing_to = if item.worry_level % self.throw_to.divisor == 0 {
-                self.throw_to.if_true
-            } else {
-                self.throw_to.if_false
-            };
-            (throwing_to, item)
-        })
+    fn process_item(&mut self, item: &mut Item) {
+        self.inspect(item);
+        item.worry_level = (item.worry_level / self.worry_div_factor) % self.worry_mod_factor;
+        item.owner = self.throw_to.throw(item);
     }
-    fn process_items(&mut self, throws: &mut Vec<(usize, Item)>) {
-        // let mut throws = Vec::with_capacity(self.items.len());
-        while let Some(throw) = self.process_item() {
-            throws.push(throw);
+    fn process_items(&mut self, items: &mut Vec<Item>) {
+        for item in items {
+            if item.owner == self.id {
+                self.process_item(item);
+            }
         }
-        // throws
     }
 }
 
@@ -133,9 +138,9 @@ impl FromStr for Monkey {
             .next()
             .and_then(|s| s.trim().strip_prefix("Monkey "))
             .ok_or(Self::Err::Monkey)
-            .and_then(|s| str::parse::<usize>(s.trim_end_matches(':')).map_err(|_| Self::Err::Monkey));
+            .and_then(|s| str::parse::<usize>(s.trim_end_matches(':')).map_err(|_| Self::Err::Monkey))?;
 
-        let items = line
+        let initial_items = line
             .next()
             .and_then(|s| s.trim().strip_prefix("Starting items:"))
             .ok_or(Self::Err::Monkey)
@@ -143,68 +148,68 @@ impl FromStr for Monkey {
                 items
                     .split(',')
                     .map(|num| {
-                        num.trim().parse::<usize>()
-                            .map(|worry_level| Item { worry_level })
+                        num.trim()
+                            .parse::<usize>()
+                            .map(|worry_level| Item { worry_level, owner: id })
                             .map_err(|_| Self::Err::Monkey)
                     })
                     .collect::<Result<Vec<_>, _>>()
-            });
-        let operation = line
+            })?;
+        let inspection = line
             .next()
             .and_then(|s| s.trim().strip_prefix("Operation: new = "))
             .ok_or(Self::Err::Monkey)
-            .and_then(Inspection::from_str);
+            .and_then(Inspection::from_str)?;
 
         let divisor = line
             .next()
             .and_then(|s| s.trim().strip_prefix("Test: divisible by "))
             .ok_or(Self::Err::Monkey)
-            .and_then(|s| s.parse::<usize>().map_err(|_| Self::Err::Monkey));
+            .and_then(|s| s.parse::<usize>().map_err(|_| Self::Err::Monkey))?;
 
         let if_true = line
             .next()
             .and_then(|s| s.trim().strip_prefix("If true: throw to monkey "))
             .ok_or(Self::Err::Monkey)
-            .and_then(|s| s.parse::<usize>().map_err(|_| Self::Err::Monkey));
+            .and_then(|s| s.parse::<usize>().map_err(|_| Self::Err::Monkey))?;
 
         let if_false = line
             .next()
             .and_then(|s| s.trim().strip_prefix("If false: throw to monkey "))
             .ok_or(Self::Err::Monkey)
-            .and_then(|s| s.parse::<usize>().map_err(|_| Self::Err::Monkey));
+            .and_then(|s| s.parse::<usize>().map_err(|_| Self::Err::Monkey))?;
 
-        if let (Ok(id), Ok(items), Ok(inspection), Ok(divisor), Ok(if_true), Ok(if_false)) =
-            (id, items, operation, divisor, if_true, if_false)
-        {
-            Ok(Self { id, items, inspection, throw_to: ThrowTo { divisor, if_true, if_false }, inspection_count: 0, worry_div_factor: 3, worry_mod_factor: usize::MAX})
-        } else {
-            Err(Self::Err::Monkey)
-        }
+        Ok(Self {
+            id,
+            initial_items,
+            inspection,
+            throw_to: ThrowTo { divisor, if_true, if_false },
+            inspection_count: 0,
+            worry_div_factor: 3,
+            worry_mod_factor: usize::MAX,
+        })
     }
 }
 
-fn parse(s: &str) -> Vec<Monkey> {
-    s.replace("\r\n", "\n")
+fn parse(s: &str) -> (Vec<Monkey>, Vec<Item>) {
+    let monkeys = s
+        .replace("\r\n", "\n")
         .split("\n\n")
         .map(|lines| Monkey::from_str(lines).unwrap_or_else(|_| panic!("Bad monkey in {lines}")))
-        .collect::<Vec<_>>()
+        .collect::<Vec<_>>();
+    let items = monkeys
+        .iter()
+        .flat_map(|m| m.initial_items.clone())
+        .collect::<Vec<_>>();
+    (monkeys, items)
 }
 
 fn part1_evaluate(s: &str) -> usize {
-    let mut monkeys = parse(s);
+    let (mut monkeys, mut items) = parse(s);
 
-    if !monkeys.iter().enumerate().all(|(i, m)| i==m.id) {
-        panic!("Monkey IDs don't match indices");
-    }
-
-    let mut throws = Vec::with_capacity(100);
-    for _cycle in 0..20 {
-        for i in 0..monkeys.len() {
-            monkeys[i].process_items(&mut throws);
-            for (throwing_to, item) in &throws {
-                monkeys[*throwing_to].items.push(*item);
-            }
-            throws.clear();
+    for _ in 0..20 {
+        for m in &mut monkeys {
+            m.process_items(&mut items);
         }
     }
     monkeys
@@ -217,22 +222,19 @@ fn part1_evaluate(s: &str) -> usize {
 }
 
 fn part2_evaluate(s: &str) -> usize {
-    let mut monkeys = parse(s);
-    let worry_mod_factor = monkeys.iter().map(|m| m.throw_to.divisor).product::<usize>();
-    monkeys.iter_mut().for_each(|m| {m.worry_div_factor=1;m.worry_mod_factor=worry_mod_factor;});
+    let (mut monkeys, mut items) = parse(s);
+    let worry_mod_factor = monkeys
+        .iter()
+        .map(|m| m.throw_to.divisor)
+        .product::<usize>();
+    monkeys.iter_mut().for_each(|m| {
+        m.worry_div_factor = 1;
+        m.worry_mod_factor = worry_mod_factor;
+    });
 
-    if !monkeys.iter().enumerate().all(|(i, m)| i==m.id) {
-        panic!("Monkey IDs don't match indices");
-    }
-
-    let mut throws = Vec::with_capacity(100);
-    for _cycle in 0..10_000 {
-        for i in 0..monkeys.len() {
-            monkeys[i].process_items(&mut throws);
-            for (throwing_to, item) in &throws {
-                monkeys[*throwing_to].items.push(*item);
-            }
-            throws.clear();
+    for _ in 0..10_000 {
+        for m in &mut monkeys {
+            m.process_items(&mut items);
         }
     }
     monkeys
